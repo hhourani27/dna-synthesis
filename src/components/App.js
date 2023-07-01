2f038dd8585a6d378765a60a891d436129ae43d0
Habib <h.hourany@gmail.com>
2023-07-01 16:09:03 +0200

    refactor: create Machine component folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Layout from "./Layout";
-import MachinesListPage from "./MachineListPage";
+import MachinesListPage from "./machine/MachineListPage";
 
 export default function MiniDrawer() {
   return (
