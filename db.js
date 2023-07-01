cd34d6589f697bfcffc10c66ed4ba5c569df5937
Habib <h.hourany@gmail.com>
2023-07-01 19:57:24 +0200

    json-server: generate dynamic data

--- db.js
+++ db.js
@@ -1,37 +1,23 @@
-module.exports = () => {
+const locations = ["Paris", "Nice"];
+
+function generateData({
+  IDLE,
+  IDLE_ASSIGNED_ORDER,
+  SYNTHETIZING,
+  WAITING_FOR_DISPATCH,
+}) {
+  const machineCount =
+    IDLE + IDLE_ASSIGNED_ORDER + SYNTHETIZING + WAITING_FOR_DISPATCH;
+
+  const machines = [...Array(machineCount).keys()].map((n) => ({
+    id: n + 1,
+    model: "DNA-SYNTH-96",
+    location: "Paris",
+    status: "IDLE",
+  }));
+
   const data = {
-    machines: [
-      {
-        id: 1,
-        model: "DNA-SYNTH-96",
-        location: "Paris",
-        status: "IDLE",
-      },
-      {
-        id: 2,
-        model: "DNA-SYNTH-96",
-        location: "Paris",
-        status: "IDLE_ASSIGNED_ORDER",
-      },
-      {
-        id: 3,
-        model: "DNA-SYNTH-96",
-        location: "Nice",
-        status: "SYNTHETIZING",
-      },
-      {
-        id: 4,
-        model: "DNA-SYNTH-96",
-        location: "Nice",
-        status: "SYNTHETIZING",
-      },
-      {
-        id: 5,
-        model: "DNA-SYNTH-96",
-        location: "Nice",
-        status: "WAITING_FOR_DISPATCH",
-      },
-    ],
+    machines: machines,
     models: [
       {
         id: "DNA-SYNTH-96",
@@ -41,4 +27,56 @@ module.exports = () => {
   };
 
   return data;
+}
+
+function generateDb() {
+  return generateData({
+    IDLE: 1,
+    IDLE_ASSIGNED_ORDER: 1,
+    SYNTHETIZING: 2,
+    WAITING_FOR_DISPATCH: 1,
+  });
+}
+
+const data = {
+  machines: [
+    {
+      id: 1,
+      model: "DNA-SYNTH-96",
+      location: "Paris",
+      status: "IDLE",
+    },
+    {
+      id: 2,
+      model: "DNA-SYNTH-96",
+      location: "Paris",
+      status: "IDLE_ASSIGNED_ORDER",
+    },
+    {
+      id: 3,
+      model: "DNA-SYNTH-96",
+      location: "Nice",
+      status: "SYNTHETIZING",
+    },
+    {
+      id: 4,
+      model: "DNA-SYNTH-96",
+      location: "Nice",
+      status: "SYNTHETIZING",
+    },
+    {
+      id: 5,
+      model: "DNA-SYNTH-96",
+      location: "Nice",
+      status: "WAITING_FOR_DISPATCH",
+    },
+  ],
+  models: [
+    {
+      id: "DNA-SYNTH-96",
+      wells: [12, 8],
+    },
+  ],
 };
+
+module.exports = generateDb;
