86427589193e8b417734f8949b500aed54405bd0
Habib <h.hourany@gmail.com>
2023-07-01 07:14:36 +0200

    Add Layout (AppBar & Drawer) from MUI template

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,114 @@
+import { useState } from "react";
+
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Drawer from "@mui/material/Drawer";
+import Divider from "@mui/material/Divider";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import MicrowaveIcon from "@mui/icons-material/Microwave";
+import ReceiptLongIcon from "@mui/icons-material/ReceiptLong";
+import AnalyticsIcon from "@mui/icons-material/Analytics";
+import InfoIcon from "@mui/icons-material/Info";
+
 import MachinesListPage from "./MachinesListPage";
 
+// function App() {
+//   return (
+//     <>
+//       <h1>Hello DNA Synthesis!</h1>
+//       <MachinesListPage />
+//     </>
+//   );
+// }
+
 function App() {
+  const [open, setOpen] = useState(true);
+
   return (
-    <>
-      <h1>Hello DNA Synthesis!</h1>
-      <MachinesListPage />
-    </>
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar position="absolute" open={open}>
+        <Toolbar sx={{ pr: "24px" }}>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ marginRight: "36px", ...(open && { display: "none" }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            component="h1"
+            variant="h6"
+            color="inherit"
+            noWrap
+            sx={{ flexGrow: 1 }}
+          >
+            DNA SYNTHESIS
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="permanent" open={open}>
+        <Toolbar
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "flex-end",
+            px: [1],
+          }}
+        >
+          <IconButton onClick={() => setOpen((open) => !open)}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </Toolbar>
+        <Divider />
+        <List component="nav">
+          <ListItem>
+            <ListItemButton>
+              <ListItemIcon>
+                <MicrowaveIcon />
+              </ListItemIcon>
+              <ListItemText primary="Machines" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton>
+              <ListItemIcon>
+                <ReceiptLongIcon />
+              </ListItemIcon>
+              <ListItemText primary="Orders" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton>
+              <ListItemIcon>
+                <AnalyticsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Dashboard" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton>
+              <ListItemIcon>
+                <InfoIcon />
+              </ListItemIcon>
+              <ListItemText primary="Onboarding" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Drawer>
+    </Box>
   );
 }
 
