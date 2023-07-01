da4af85ae0676401c7cb2703aef4c3d6da8bab06
Habib <h.hourany@gmail.com>
2023-07-01 10:38:18 +0200

    Use MUI for MachineTable

--- src/components/MachinesTable.js
+++ src/components/MachinesTable.js
@@ -1,22 +1,32 @@
+import TableContainer from "@mui/material/TableContainer";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import Paper from "@mui/material/Paper";
+
 export default function MachinesTable({ machines }) {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Model</th>
-          <th>Location</th>
-        </tr>
-      </thead>
-      <tbody>
-        {machines.map((m) => (
-          <tr key={m.id}>
-            <td>{m.id}</td>
-            <td>{m.model}</td>
-            <td>{m.location}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="machines table">
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>Model</TableCell>
+            <TableCell>Location</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {machines.map((m) => (
+            <TableRow key={m.id}>
+              <TableCell>{m.id}</TableCell>
+              <TableCell>{m.model}</TableCell>
+              <TableCell>{m.location}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
