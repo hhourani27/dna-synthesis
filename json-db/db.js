4a88c2023631398f9756ade9691ba2973bc5ed7b
Habib <h.hourany@gmail.com>
2023-07-02 06:57:14 +0200

    json-db : generate current db with js

--- json-db/db.js
+++ json-db/db.js
@@ -1,22 +1,38 @@
 var Chance = require("chance");
-var chance = new Chance();
+var chance = new Chance(27);
 
 const locations = ["Paris", "Nice"];
 
-function generateData({
+/**
+ * Generate the whole database containing machines & orders given the machine count per status as a paramater
+ * @param {int} IDLE : Number of machines having the state IDLE
+ * @param {int} IDLE_ASSIGNED_ORDER : Number of machines having the state IDLE_ASSIGNED_ORDER
+ * @param {int} SYNTHETIZING : Number of machines having the state SYNTHETIZING
+ * @param {int} WAITING_FOR_DISPATCH : Number of machines having the state WAITING_FOR_DISPATCH
+ */
+const generateData = ({
   IDLE,
   IDLE_ASSIGNED_ORDER,
   SYNTHETIZING,
   WAITING_FOR_DISPATCH,
-}) {
+}) => {
+  // Count total number of machines
   const machineCount =
     IDLE + IDLE_ASSIGNED_ORDER + SYNTHETIZING + WAITING_FOR_DISPATCH;
 
-  const machines = [...Array(machineCount).keys()].map((n) => ({
-    id: n + 1,
+  // An array of size machineCount, containing the machine statuses
+  const machineStatuses = chance.shuffle([
+    ...Array(IDLE).fill("IDLE"),
+    ...Array(IDLE_ASSIGNED_ORDER).fill("IDLE_ASSIGNED_ORDER"),
+    ...Array(SYNTHETIZING).fill("SYNTHETIZING"),
+    ...Array(WAITING_FOR_DISPATCH).fill("WAITING_FOR_DISPATCH"),
+  ]);
+
+  const machines = machineStatuses.map((status, i) => ({
+    id: i + 1,
     model: "DNA-SYNTH-96",
-    location: chance.string(),
-    status: "IDLE",
+    location: chance.pickone(locations),
+    status: status,
   }));
 
   const data = {
@@ -30,56 +46,17 @@ function generateData({
   };
 
   return data;
-}
+};
 
-function generateDb() {
-  return generateData({
+/**
+ * Generate the whole databae
+ */
+const generateDb = () =>
+  generateData({
     IDLE: 1,
     IDLE_ASSIGNED_ORDER: 1,
     SYNTHETIZING: 2,
     WAITING_FOR_DISPATCH: 1,
   });
-}
-
-const data = {
-  machines: [
-    {
-      id: 1,
-      model: "DNA-SYNTH-96",
-      location: "Paris",
-      status: "IDLE",
-    },
-    {
-      id: 2,
-      model: "DNA-SYNTH-96",
-      location: "Paris",
-      status: "IDLE_ASSIGNED_ORDER",
-    },
-    {
-      id: 3,
-      model: "DNA-SYNTH-96",
-      location: "Nice",
-      status: "SYNTHETIZING",
-    },
-    {
-      id: 4,
-      model: "DNA-SYNTH-96",
-      location: "Nice",
-      status: "SYNTHETIZING",
-    },
-    {
-      id: 5,
-      model: "DNA-SYNTH-96",
-      location: "Nice",
-      status: "WAITING_FOR_DISPATCH",
-    },
-  ],
-  models: [
-    {
-      id: "DNA-SYNTH-96",
-      wells: [12, 8],
-    },
-  ],
-};
 
 module.exports = generateDb;
