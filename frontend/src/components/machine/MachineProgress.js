b23531362122642245703a34c5dece5d8ad574e2
Habib <h.hourany@gmail.com>
2023-07-04 17:43:28 +0200

    Display #cycles and step icons

--- frontend/src/components/machine/MachineProgress.js
+++ frontend/src/components/machine/MachineProgress.js
@@ -1,23 +1,63 @@
 import Box from "@mui/material/Box";
 import Well from "./Well";
 
+import SwitchAccessShortcutAddIcon from "@mui/icons-material/SwitchAccessShortcutAdd";
+import RemoveModeratorIcon from "@mui/icons-material/RemoveModerator";
+import ShowerIcon from "@mui/icons-material/Shower";
+
 export default function MachineProgress({
   status,
   completedCycles,
   totalCycles,
   currentStep,
 }) {
-  const size = 200;
+  const size = 175;
 
-  return status === "IDLE" ? (
-    <Well idle size={size} />
-  ) : (
-    <Well
-      size={size}
-      status={status}
-      completedCycles={completedCycles}
-      totalCycles={totalCycles}
-      currentStep={currentStep}
-    />
+  return (
+    <Box display="flex" flexDirection="column" gap={1}>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <RemoveModeratorIcon />
+      </Box>
+      <Box display="flex" flexDirection="row" gap={1}>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <SwitchAccessShortcutAddIcon />
+        </Box>
+        <Box>
+          {status === "IDLE" ? (
+            <Well idle size={size} />
+          ) : (
+            <Well
+              size={size}
+              status={status}
+              completedCycles={completedCycles}
+              totalCycles={totalCycles}
+              currentStep={currentStep}
+              displayCount
+            />
+          )}
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <ShowerIcon />
+        </Box>
+      </Box>
+    </Box>
   );
 }
--- frontend/src/components/machine/Well.js
+++ frontend/src/components/machine/Well.js
@@ -2,11 +2,13 @@ import CircularProgress, {
   circularProgressClasses,
 } from "@mui/material/CircularProgress";
 import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 export default function Well({
   idle = false,
   completedCycles = 0,
   totalCycles = 0,
+  displayCount = false,
   size = 40,
 }) {
   const thickness = 8;
@@ -39,6 +41,31 @@ export default function Well({
           size={size}
           value={(100 * completedCycles) / totalCycles}
         />
+      )}
+      {displayCount && (
+        <Box
+          sx={{
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0,
+            position: "absolute",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Typography
+            variant="caption"
+            component="div"
+            color="text.secondary"
+            align="center"
+          >
+            <span>{`${completedCycles} / ${totalCycles}`}</span>
+            <br />
+            <span>cycles</span>
+          </Typography>
+        </Box>
       )}
     </Box>
   );
