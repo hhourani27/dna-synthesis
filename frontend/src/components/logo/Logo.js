8ab519011a6b371b73910f8d6c60fc51851b2d51
Habib <h.hourany@gmail.com>
2023-07-09 17:56:31 +0200

    Correct nowrap for logo

--- frontend/src/components/logo/Logo.js
+++ frontend/src/components/logo/Logo.js
@@ -7,17 +7,22 @@ const LogoAndText = styled("div")(({ theme }) => ({
   flexDirection: "row",
   alignItems: "center",
   gap: theme.spacing(1),
+  flexWrap: "nowrap",
 }));
 
 const BlackLogo = styled("img")(({ theme }) => ({
   filter: "invert(1)",
 }));
 
+const LogoText = styled("span")(({ theme }) => ({
+  whiteSpace: "nowrap",
+}));
+
 export default function Logo({ open }) {
   return (
     <LogoAndText>
       <BlackLogo src={dnaLogo} alt="Logo" width="40" />
-      {open && <div>DNA SYNTHESIS</div>}
+      {open && <LogoText>DNA SYNTHESIS</LogoText>}
     </LogoAndText>
   );
 }
