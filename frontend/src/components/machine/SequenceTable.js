84f378c4c5e86221a47d9eefb2af9acf6d60f864
Habib <h.hourany@gmail.com>
2023-07-10 18:45:08 +0200

    SequenceTable: use grid

--- frontend/src/components/machine/SequenceTable.js
+++ frontend/src/components/machine/SequenceTable.js
@@ -1,44 +1,66 @@
+import { Fragment } from "react";
 import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+
 import Well from "./Well";
 
-const Table = styled("table")(({ theme }) => ({
-  "& td, & th": {
-    padding: `${theme.spacing(1)} 0`,
-    textAlign: "center",
+const GridSequenceTable = styled("div")(({ theme, wellArraySize }) => ({
+  display: "grid",
+  gridTemplateRows: `repeat(${wellArraySize},1fr)`,
+  alignItems: "center",
+
+  letterSpacing: 7,
+
+  "& .progress": {
+    paddingRight: 16,
   },
 
-  "& th": {
-    paddingRight: theme.spacing(2),
+  "& .completed": {
+    color: theme.palette.success.main,
+    fontWeight: 600,
   },
 
-  "& td.nucleotide": {
-    color: theme.palette.text.secondary,
-    fontWeight: 400,
-  },
-
-  "& td.nucleotide.synthetized": {
+  "& .synthetized": {
     color: theme.palette.secondary.main,
     fontWeight: 600,
   },
 
-  "& td.nucleotide.completed": {
-    color: theme.palette.success.main,
-    fontWeight: 600,
+  "& .non-synthetized": {
+    color: theme.palette.text.secondary,
+    fontWeight: 400,
   },
 }));
 
+const splitOligo = (oligo, completedCycles) => ({
+  synthethizedNucleotides: oligo.substring(0, completedCycles),
+  nonSynthethizedNucleotides: oligo.substring(completedCycles),
+});
+
 export default function SequenceTable({ wellArraySize, wells }) {
-  const maxLenghtOligo = Math.max(...wells.map((w) => w.totalCycles));
-  const oligoIsCompleted = wells.map(
-    (w) => w.synthetizedNucleotideCount === w.totalCycles
-  );
+  const wellsDisplay = wells.map((w) => {
+    const { synthethizedNucleotides, nonSynthethizedNucleotides } = splitOligo(
+      w.oligo,
+      w.synthetizedNucleotideCount
+    );
+
+    const oligoIsCompleted = w.synthetizedNucleotideCount === w.totalCycles;
+
+    return {
+      synthethizedNucleotides,
+      nonSynthethizedNucleotides,
+      oligoIsCompleted,
+      ...w,
+    };
+  });
 
   return (
-    <Table>
-      <tbody>
-        {wells.map((w, widx) => (
-          <tr key={w.id}>
-            <th>
+    <GridSequenceTable>
+      {wellsDisplay.map((w) => {
+        const gRow = w.id + 1;
+
+        return (
+          <Fragment key={w.id}>
+            <Box sx={{ gridRow: gRow, gridColumn: 1 }} className="progress">
               {w.status === "IDLE" ? (
                 <Well idle />
               ) : (
@@ -48,24 +70,22 @@ export default function SequenceTable({ wellArraySize, wells }) {
                   totalCycles={w.totalCycles}
                 />
               )}
-            </th>
-            {[...Array(maxLenghtOligo).keys()].map((n) => (
-              <td
-                key={n}
-                className={`nucleotide ${
-                  oligoIsCompleted[widx]
-                    ? "completed"
-                    : n < w.synthetizedNucleotideCount
-                    ? "synthetized"
-                    : ""
-                }`}
-              >
-                {n < w.oligo.length ? w.oligo.charAt(n) : " "}
-              </td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </Table>
+            </Box>
+            <Box
+              sx={{ gridRow: gRow, gridColumn: 2 }}
+              className={w.oligoIsCompleted ? "completed" : "synthetized"}
+            >
+              {w.synthethizedNucleotides}
+            </Box>
+            <Box
+              sx={{ gridRow: gRow, gridColumn: 3 }}
+              className="non-synthetized"
+            >
+              {w.nonSynthethizedNucleotides}
+            </Box>
+          </Fragment>
+        );
+      })}
+    </GridSequenceTable>
   );
 }
