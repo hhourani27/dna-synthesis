9855e758b55228a50101206fb203235a30c8e9c0
Habib <h.hourany@gmail.com>
2023-07-10 13:59:10 +0200

    hange color for completed Nucleotides

--- frontend/src/components/machine/SequenceTable.js
+++ frontend/src/components/machine/SequenceTable.js
@@ -1,15 +1,36 @@
 import Box from "@mui/material/Box";
+import { styled } from "@mui/material/styles";
+
+const Nucleotide = styled("td")(({ theme, n, synthetized }) => ({
+  color: synthetized
+    ? theme.palette.success.main
+    : theme.palette.text.secondary,
+  fontWeight: synthetized ? 600 : 400,
+}));
 
 export default function SequenceTable({ wellArraySize, wells }) {
   const maxLenghtOligo = Math.max(...wells.map((w) => w.totalCycles));
 
   return (
-    <Box component="table">
+    <Box
+      component="table"
+      // sx={{
+      //   border: "1px solid",
+      //   "& td": {
+      //     border: "1px solid",
+      //   },
+      // }}
+    >
       <tbody>
         {wells.map((w) => (
           <tr key={w.id}>
             {[...Array(maxLenghtOligo).keys()].map((n) => (
-              <td key={n}>{n < w.oligo.length ? w.oligo.charAt(n) : " "}</td>
+              <Nucleotide
+                key={n}
+                synthetized={n < w.synthetizedNucleotideCount}
+              >
+                {n < w.oligo.length ? w.oligo.charAt(n) : " "}
+              </Nucleotide>
             ))}
           </tr>
         ))}
