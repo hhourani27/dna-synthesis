21c811ed71e0ee41914c86b21603e4cd0f0e7782
Habib <h.hourany@gmail.com>
2023-07-11 08:16:02 +0200

    WellSequences: add header bar

--- frontend/src/components/machine/WellSequencesCard.js
+++ frontend/src/components/machine/WellSequencesCard.js
@@ -2,6 +2,7 @@ import WellSequences from "./WellSequences";
 import { styled } from "@mui/material/styles";
 import TextField from "@mui/material/TextField";
 import ToggleButton from "@mui/material/ToggleButton";
+import SortIcon from "@mui/icons-material/Sort";
 
 const WellSequencesContainer = styled("div")(({ theme }) => ({
   display: "flex",
@@ -12,12 +13,30 @@ const WellSequencesContainer = styled("div")(({ theme }) => ({
 const WellSequencesBar = styled("div")(({ theme }) => ({
   display: "flex",
   flexDirection: "row",
+  justifyContent: "space-between",
+
+  backgroundColor: theme.palette.background.paper,
+  zIndex: theme.zIndex.appBar,
+  position: "sticky",
+  top: 0,
 }));
 
 export default function WellSequencesCard({ wells, selectedWellId }) {
   return (
     <WellSequencesContainer>
-      <WellSequencesBar>Header</WellSequencesBar>
+      <WellSequencesBar>
+        <TextField id="sequence-search" label="ATCG..." variant="standard" />
+        <ToggleButton
+          value="check"
+          size="small"
+          //   selected={selected}
+          //   onChange={() => {
+          //     setSelected(!selected);
+          //   }}
+        >
+          <SortIcon />
+        </ToggleButton>
+      </WellSequencesBar>
       <WellSequences wells={wells} selectedWellId={selectedWellId} />
     </WellSequencesContainer>
   );
