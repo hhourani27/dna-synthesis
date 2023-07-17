d3164ffd788b2cbde46d30aa87b6d10397bcb14b
Habib <h.hourany@gmail.com>
2023-07-17 10:49:24 +0200

    lowercase status enum for Machine

--- backend-ruby/app/models/machine.rb
+++ backend-ruby/app/models/machine.rb
@@ -1,3 +1,3 @@
 class Machine < ApplicationRecord
-    enum :status, {IDLE: 0, IDLE_ASSIGNED_ORDER: 1, SYNTHETIZING: 2, WAITING_FOR_DISPATCH: 3}
+    enum :status, {idle: 0, idle_assigned_order: 1, synthetizing: 2, waiting_for_dispatch: 3}
 end
