9b6149c9df8fd2361b445ae0c7cb876842c23562
Habib <h.hourany@gmail.com>
2023-07-21 08:44:41 +0200

    add route GET /machines/:id

--- backend-ruby/app/controllers/machines_controller.rb
+++ backend-ruby/app/controllers/machines_controller.rb
@@ -3,4 +3,9 @@ class MachinesController < ApplicationController
     @machines = Machine.all
     render json: @machines.map(&:render_json)
   end
+
+  def show
+    @machine = Machine.find(params[:id])
+    render json: @machine.render_json
+  end
 end
