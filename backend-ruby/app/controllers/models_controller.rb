ce507c38fcd382b2fba50fbfbc1c6c4616b711d4
Habib <h.hourany@gmail.com>
2023-07-21 08:54:50 +0200

    add route for GET /models/:id

--- backend-ruby/app/controllers/models_controller.rb
+++ backend-ruby/app/controllers/models_controller.rb
@@ -3,4 +3,9 @@ class ModelsController < ApplicationController
     @models = Model.all
     render json: @models.map(&:render_json)
   end
+
+  def show
+    @model = Model.find_by(name: params[:id])
+    render json: @model.render_json
+  end
 end
