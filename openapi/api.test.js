82d369406270dee798c8a67f2471e379f2a0643b
Habib <h.hourany@gmail.com>
2023-07-24 07:51:52 +0200

    create first api test

--- openapi/api.test.js
+++ openapi/api.test.js
@@ -1,5 +1,26 @@
-const request = require("supertest");
+/**
+ * Added those 2 lines, as fetching was failing with TypeError: fetch failed  connect ECONNREFUSED ::1:3001
+ * Although, Node does not fully support fetch, it does when there's "type": "module" in package.json
+ * But even after adding "type": "module", it still fails.
+ *
+ * This seems to be a dns issue with using fetch. I added those 2 lines to solve it following https://stackoverflow.com/a/72416352/471461
+ */
+const dns = require("node:dns");
+dns.setDefaultResultOrder("ipv4first");
 
-test("Test test", () => {
-  expect(true).toBe(true);
+const SERVER_URL = "http://localhost:3001/";
+
+describe("GET /machines", () => {
+  test("id, model, location, status & wells are always present", async () => {
+    const response = await fetch(SERVER_URL + "machines");
+    const machines = await response.json();
+    machines.forEach((m) => {
+      expect(m).toMatchObject({
+        id: expect.any(Number),
+        model: expect.any(String),
+        location: expect.any(String),
+        status: expect.any(String),
+      });
+    });
+  });
 });
